23967e25e4680635f39c55127284f847219249d1
Adam Koch <adam@dogtired.org>
2017-04-20 23:06:10 -0500

    more comments in Statement start conversion to Gradle

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Learning Record Store (LRS) Implementation of my API (lrs-api)
+# Learning Record Store (LRS) Implementation of my API (lrs-api)
+
+## Work in Progress
+
+This is a work in progress
--- src/main/java/com/adamkoch/lrs/Statement.java
+++ src/main/java/com/adamkoch/lrs/Statement.java
@@ -8,6 +8,11 @@ import java.util.Collection;
 /**
  * <a href="https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#20-statements">Statement</a>.
  *
+ * <p>"Statements are the evidence for any sort of experience or event which is to be tracked in xAPI. While Statements
+ * follow a machine readable JSON format, they can also easily be described using natural language. This can be
+ * extremely useful for the design process. Statements are meant to be aggregated and analyzed to provide larger meaning
+ * for the overall experience than just the sum of its parts."</p>
+ *
  * <p>Created by aakoch on 2017-03-11.</p>
  *
  * @author aakoch
@@ -19,9 +24,14 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
 
     private LocalDateTime timestamp;
 
+    /**
+     * A Statement MUST use "actor", "verb", and "object".
+     */
     private Actor actor;
 
     /**
+     * A Statement MUST use "actor", "verb", and "object".
+     *
      * Valid values are: Activity, Agent, Group, SubStatement or StatementRef
      */
     private LrsObject object;
@@ -39,6 +49,10 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
 
     private Collection attachments;
 
+
+    /**
+     * A Statement MUST use "actor", "verb", and "object".
+     */
     private com.adamkoch.lrs.api.Verb verb;
 
     /**
@@ -46,6 +60,10 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
      */
     private String version;
 
+
+    /**
+     * ID is an exception to the immutability rule.
+     */
     public void setStatementId(StatementId statementId) {
         this.statementId = statementId;
     }
@@ -54,6 +72,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return statementId;
     }
 
+    /**
+     * Timestamp is an exception to the immutability rule.
+     */
     public void setTimestamp(LocalDateTime timestamp) {
         this.timestamp = timestamp;
     }
@@ -62,6 +83,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return timestamp;
     }
 
+    /**
+     * Does the immutability rule mean there should be no setter?
+     */
     public void setActor(Actor actor) {
         this.actor = actor;
     }
@@ -74,6 +98,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return object;
     }
 
+    /**
+     * Does the immutability rule mean there should be no setter?
+     */
     public void setObject(LrsObject object) {
         this.object = object;
     }
@@ -82,6 +109,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return verb;
     }
 
+    /**
+     * Does the immutability rule mean there should be no setter?
+     */
     public void setVerb(com.adamkoch.lrs.api.Verb verb) {
         this.verb = verb;
     }
@@ -90,6 +120,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return result;
     }
 
+    /**
+     * Does the immutability rule mean there should be no setter?
+     */
     public void setResult(Result result) {
         this.result = result;
     }
@@ -98,6 +131,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return context;
     }
 
+    /**
+     * Does the immutability rule mean there should be no setter?
+     */
     public void setContext(Context context) {
         this.context = context;
     }
@@ -106,6 +142,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return authority;
     }
 
+    /**
+     * Authority is an exception to the immutability rule.
+     */
     public void setAuthority(Authority authority) {
         this.authority = authority;
     }
@@ -114,6 +153,9 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return version;
     }
 
+    /**
+     * Version is an exception to the immutability rule.
+     */
     public void setVersion(String version) {
         this.version = version;
     }
@@ -122,7 +164,71 @@ public class Statement implements com.adamkoch.lrs.api.Statement {
         return stored;
     }
 
+    /**
+     * Stored is an exception to the immutability rule.
+     */
     public void setStored(LocalDateTime stored) {
         this.stored = stored;
+    }
+
+    public Collection getAttachments() {
+        return attachments;
+    }
+
+    /**
+     * Attachments are an exception to the immutability rule.
+     */
+    public void setAttachments(Collection attachments) {
+        this.attachments = attachments;
+    }
+
+    /**
+     * There are a number of scenarios outlined in this specification which require Statements to be compared to see if
+     * they match. In these scenarios, the following rules apply:
+     *
+     * Differences which could have been caused by exceptions to Statement immutability MUST be ignored. Differences
+     * relating to a different serialization of any properties not listed as exceptions MUST not be ignored.
+     */
+    @Override
+    public boolean equals(Object o) {
+        // TODO: equals method does not yet conform to Javadoc statement
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Statement statement = (Statement) o;
+
+        if (statementId != null ? !statementId.equals(statement.statementId) : statement.statementId != null)
+            return false;
+        if (timestamp != null ? !timestamp.equals(statement.timestamp) : statement.timestamp != null) return false;
+        if (actor != null ? !actor.equals(statement.actor) : statement.actor != null) return false;
+        if (object != null ? !object.equals(statement.object) : statement.object != null) return false;
+        if (result != null ? !result.equals(statement.result) : statement.result != null) return false;
+        if (context != null ? !context.equals(statement.context) : statement.context != null) return false;
+        if (stored != null ? !stored.equals(statement.stored) : statement.stored != null) return false;
+        if (authority != null ? !authority.equals(statement.authority) : statement.authority != null) return false;
+        if (attachments != null ? !attachments.equals(statement.attachments) : statement.attachments != null)
+            return false;
+        if (verb != null ? !verb.equals(statement.verb) : statement.verb != null) return false;
+        return version != null ? version.equals(statement.version) : statement.version == null;
+    }
+
+    /**
+     * @see #equals(Object)
+     */
+    @Override
+    public int hashCode() {
+        // TODO: hashCode method does not yet conform to Javadoc statement
+        int result1 = statementId != null ? statementId.hashCode() : 0;
+        result1 = 31 * result1 + (timestamp != null ? timestamp.hashCode() : 0);
+        result1 = 31 * result1 + (actor != null ? actor.hashCode() : 0);
+        result1 = 31 * result1 + (object != null ? object.hashCode() : 0);
+        result1 = 31 * result1 + (result != null ? result.hashCode() : 0);
+        result1 = 31 * result1 + (context != null ? context.hashCode() : 0);
+        result1 = 31 * result1 + (stored != null ? stored.hashCode() : 0);
+        result1 = 31 * result1 + (authority != null ? authority.hashCode() : 0);
+        result1 = 31 * result1 + (attachments != null ? attachments.hashCode() : 0);
+        result1 = 31 * result1 + (verb != null ? verb.hashCode() : 0);
+        result1 = 31 * result1 + (version != null ? version.hashCode() : 0);
+        return result1;
     }
 }
